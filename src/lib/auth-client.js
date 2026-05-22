3031ecbebb74da582acdfa3fee5b6e3b63183ae7
sabbirhossain778 <mshb5778@gmail.com>
2026-05-22 22:01:45 +0600

    Update authClient

--- src/lib/auth-client.js
+++ src/lib/auth-client.js
@@ -1,7 +1,7 @@
 import { createAuthClient } from "better-auth/react"
 import { jwtClient } from "better-auth/client/plugins";
 export const authClient = createAuthClient({
-    baseURL: process.env.BETTER_AUTH_URL,
+    baseURL: process.env.BETTER_AUTH_URL || process.env.NEXT_PUBLIC_BETTER_AUTH_URL ,
     plugins: [
     jwtClient()
   ]
